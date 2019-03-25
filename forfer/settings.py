7133376024f8b6df94c92693f4f24c66312a01b0
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-03-25 23:07:33 +0700

    updated allowed host to heroku

--- forfer/settings.py
+++ forfer/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', 'forfer.com']
+ALLOWED_HOSTS = ['localhost', 'forfer.herokuapp.com']
 
 
 # Application definition
