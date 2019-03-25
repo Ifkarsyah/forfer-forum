a0f55fdace9bde8791f711de4b9026fe7fd2f8b8
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-03-25 08:15:41 +0700

    updated settings.py

--- forfer/settings.py
+++ forfer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$o=%$m2t%4gbai8jydx$1i9u38^09u&g!+0g_o&wje5(o*7e=7'
+SECRET_KEY = config(
+    'SECRET_KEY', default='$o=%$m2t%4gbai8jydx$1i9u38^09u&g!+0g_o&wje5(o*7e=7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
