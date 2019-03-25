adb385cef1ca8fe630beedd2c171fb5e65fb5f85
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-03-26 00:09:13 +0700

    remove whitenoise from wsgi

--- forfer/wsgi.py
+++ forfer/wsgi.py
@@ -9,11 +9,7 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 
 import os
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forfer.settings')
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 application = get_wsgi_application()
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'static'))
-application.add_files(os.path.join(BASE_DIR, 'static'), prefix='more-files/')
