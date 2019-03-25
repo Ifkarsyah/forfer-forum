d6512e0161d0839fdd046d2a964a3ca2fa8dc617
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-03-25 09:38:00 +0700

    added first form

--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, HttpResponse
-from django.http import Http404
-from .models import Board
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect, get_object_or_404, Http404
+from .forms import NewTopicForm
+from .models import Board, Topic, Post
 
 
 def home(request):
@@ -14,3 +15,25 @@ def board_topics(request, pk):
     except Board.DoesNotExist:
         raise Http404
     return render(request, 'topics.html', {'board': board})
+
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()  # TODO: get the currently logged in user
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            # TODO: redirect to the created topic page
+            return redirect('board_topics', pk=board.pk)
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
--- forfer/urls.py
+++ forfer/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from boards import views
 
+# Warning:  always don't start /, but end with /
 urlpatterns = [
     path('', views.home, name='home'),
     path('boards/<int:pk>/', views.board_topics, name='board_topics'),
+    path('boards/<int:pk>/new/', views.new_topic, name='new_topic'),
     path('admin/', admin.site.urls),
 ]
