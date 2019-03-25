bbb2caf3ff6bc1b1cf6950571842cc24a27946e9
Ferdian Ifkarsyah <ferdian.ifkarsyah@gmail.com>
2019-03-25 22:12:37 +0700

    updated requirements

--- forfer/settings.py
+++ forfer/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost', 'forfer.com']
 
 
 # Application definition
